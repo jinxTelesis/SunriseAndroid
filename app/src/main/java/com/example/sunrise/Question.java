4cce52a6135d5f030b1fddb8a5dbbc2829d37c46
Andre Lussier <Dremon7@hotmail.com>
2019-09-02 19:49:48 -0400

    implemented Recyclerview and data classes

---
+++ app/src/main/java/com/example/sunrise/Question.java
@@ -0,0 +1,17 @@
+package com.example.sunrise;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Question {
+
+    public String title;
+    public String body;
+
+    @SerializedName("question_id")
+    public String questionId;
+
+    @Override
+    public String toString(){
+        return(title);
+    }
+}
